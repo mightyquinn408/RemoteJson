2509e193ecc9662148d809bf6c0cdcd853793337
mightyquinn408 <mityquinn@me.com>
2022-02-28 15:20:50 -0700

    Initial Commit

---
+++ RemoteJSON/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RemoteJSON
+//
+//  Created by Michael Quinn on 2/28/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RemoteJSON/RemoteJSONApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemoteJSONApp.swift
+//  RemoteJSON
+//
+//  Created by Michael Quinn on 2/28/22.
+//
+
+import SwiftUI
+
+@main
+struct RemoteJSONApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
