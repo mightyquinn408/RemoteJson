4ca9c65164d0e57cb72645b6fc999e5ba7e7b353
mightyquinn408 <mityquinn@me.com>
2022-02-28 16:38:33 -0700

    Added Async Image

--- RemoteJSON/ContentView.swift
+++ RemoteJSON/ContentView.swift
@@ -21,14 +21,29 @@ struct ContentView: View {
     @State private var results = [Result]()
 
     var body: some View {
-        List(results, id: \.trackId) { item in
-            VStack(alignment: .leading) {
-                Text(item.trackName)
-                    .font(.headline)
-                Text(item.collectionName)
+        VStack {
+            AsyncImage(url: URL(string: "https://hws.dev/img/logo.png")) { phase in
+                if let image = phase.image {
+                    image
+                        .resizable()
+                        .scaledToFit()
+                } else if phase.error != nil {
+                    Text("There was an error loading the image.")
+                } else {
+                    ProgressView()
+                }
             }
-        }.task {
-            await loadData()
+            .frame(width: 200, height: 200)
+            
+            List(results, id: \.trackId) { item in
+                VStack(alignment: .leading) {
+                    Text(item.trackName)
+                        .font(.headline)
+                    Text(item.collectionName)
+                }
+            }.task {
+                await loadData()
+        }
         }
     }
     
